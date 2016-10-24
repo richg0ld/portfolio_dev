2895df0c6271b01cae3dc00d24945530c9cba84c
Richg0ld <corosk@gmail.com>
2016-10-24 22:49:18 +0900

    데이터 내용 수정

--- src/components/worksData.js
+++ src/components/worksData.js
@@ -6,8 +6,8 @@ export const worksData =[{
     startDate: 201301,
     finishDate: 201503,
     type:"W",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:''
+    role:'마크업 작업 및 유지보수',
+    desc:'엑스퍼트 컨설팅의 메인 홈페이지로서, 진행되고 있는 다양한 교육 또는 세미나를 알리거나 신처을 받는 곳이며, 메인페이지의 베너 슬라이더와 팝업에 Jquery코드를 사용하여 유지보수를 하였습니다.'
 
 }, {
     name: '뷰웍스 채용 사이트',
@@ -17,8 +17,8 @@ export const worksData =[{
     startDate: 201408,
     finishDate: 201509,
     type:"W",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:''
+    role:'마크업과 Javascript, Jquery를 사용한 프론트 개발',
+    desc:'뷰웍스의 채용 사이트로서 구직자의 인재풀을 등록할수 있고 진행중인 채용정보를 알 수 있는 웹사이트이다. 마크업과 페이지의 UI부분을 전부 담당하였스며, ie7까지 호환성을 맞추었으며 UI적 기능이나 애니메이션부분은 Jquery를 사용하였습니다.'
 }, {
     name: '행복한휴일',
     url: 'http://www.happyholiday.co.kr/',
@@ -27,8 +27,8 @@ export const worksData =[{
     startDate: 201404,
     finishDate: 201505,
     type:"W",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:''
+    role:'마크업과 Jquery를 사용한 프론트 개발',
+    desc:'엑스퍼트연수원의 일반인 예약 사이트이며, ie6까지 호환이되고 Jquery를 사용하여 메뉴의 UI기능을 작업 하였습니다.'
 }, {
     name: '엑스퍼트 대부도 연수원',
     url: 'http://www.expertacademy.co.kr/',
@@ -37,8 +37,8 @@ export const worksData =[{
     startDate: 201311,
     finishDate: 201401,
     type:"W",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:''
+    role:'마크업과 Jquery를 사용한 프론트 개발',
+    desc:'엑스퍼트연수원 홍보및 예약 사이트로서, ie8까지 호환이 가능하며, 슬라이드 라이브러리들을 회사에 맞게 커스터마이징 하여 수정하여 사용 하였스며, Jquery와 순수 Javascript를 이용 하여 작업하였습니다.'
 }, {
     name: 'sk텔링크 WA인증마크 획득',
     url: 'http://www.sktelink.co.kr',
@@ -47,28 +47,28 @@ export const worksData =[{
     startDate: 201504,
     finishDate: 201505,
     type:"WM",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:''
+    role:'마크업과 Jquery, Javascript를 이용한 접근성 개선',
+    desc:'sk탤링크 홈페이지의 웹접근성을 개선하기위해 탭이동, 메인슬라이드에서의 포커스 위치에 따른 슬라이더의 스크립트 자동 실행, 취소 팝업 페이지와의 연동등의 개선을 통하여 웹접근성 인증마크를 획득 하였습니다.'
 }, {
-    name: 'kt금호렌터카에서 롯데렌터카로 리브랜딩 작업',
+    name: 'KT금호렌터카에서 롯데렌터카로 리브랜딩 작업',
     url: 'http://www.lotterentacar.net',
     img: require('./../images/nara.gif'),
     thumbImg: '',
     startDate: 201506,
     finishDate: 201507,
     type:"WM",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:''
+    role:'마크업과 Jquery를 사용한 프론트 개발',
+    desc:'기존 KT금호렌터카의 내용들과 이미지 모두 수정하고, 페인페이지와, 렌트신청페이지 같은 주요페이지의 기능들을 개선 하였습니다.'
 }, {
-    name: '한빛온 공식 웹사이트 리뉴얼',
+    name: '한빛온 공식 웹사이트 리뉴얼 및 공통 GNB 리뉴얼',
     url: 'http://www.hanbiton.com/',
     img: require('./../images/nara.gif'),
     thumbImg: '',
     startDate: 201604,
     finishDate: 201605,
-    type:"WM",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:'회사내에서 처음으로 canvas에 순수 자바스크립트를 사용하여 인트로 페이지의 애니메이션 제작 기존에 모듈화 하였던 티징 페이지 코드를 좀더 수정하여 기능과 선언의 분리를 좀더 명확하게 함 코드의 관리가 좀더 쉬워짐'
+    type:"W",
+    role:'마크업, Jquery, javascript를 이용한 프론트 인터렉션 개발',
+    desc:'회사에서 사용할 수 있게 제작한 슬라이드 라이브러리 첫 사용한 첫 개발이며, 메인슬라이더 및 이벤트 리스트 같은 서버 사이드 렌더링이 필요한 부분에 간단한 메소드를 제공하여 협업 프로그래머와 책임과 담당을 효율적으로 분리하여 작업하였습니다. 공통 GNB의 경우 회사 내부 사정으로 Jquery를 사용할 수 없는 상황이었기 때문에 순수 자바스크립트로 분기처리를 이용해 하위 브라우저까지의 지원을 가능하게 하였습니다.'
 }, {
     name: '그라나도 에스파다 일리에 2차 티저 페이지 제작',
     url: 'http://ge.hanbiton.com/event/2016/02/20160211xlwl/event.aspx',
@@ -77,8 +77,8 @@ export const worksData =[{
     startDate: 201601,
     finishDate: 201602,
     type:"P",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:''
+    role:'마크업, Jquery, javascript를 이용한 프론트 인터렉션 개발',
+    desc:'첫 게임 프로모션 페이지 작업이며, 다양한 인터렉션 요소로 수 많은 돔조작으로 인한 렌더링 이슈를 경험하게 되었으며, 이를 해결하기위한 모듈화 작업의 첫 예시라고 볼수있습니다. ie8의 지원을 위해 es5코드 없이 es3.1의 코드를 이용하여 작업을 하였습니다.'
 }, {
     name: '그라나도 에스파다 오르덴 2차 티저 페이지 제작',
     url: 'http://ge.hanbiton.com/event/2016/07/20160707dhfm/event.aspx',
@@ -87,8 +87,8 @@ export const worksData =[{
     startDate: 201606,
     finishDate: 201607,
     type:"P",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:''
+    role:'canvas library, javascript를 이용한 프론트 인터렉션 개발',
+    desc:'canvas library를 이용하여 메인씬의 인터렉션을 구현하였으며, 페이지들의 공통된 부분을 클래스패턴을 이용하여 처리해 중복되는 코드들을 제거 하기위해 노력하고 코드레벨에서의 명확성을 주기위해 노력한 작업입니다.'
 }, {
     name: '그라나도 에스파다 오르덴 3차 티저 페이지 제작',
     url: 'http://ge.hanbiton.com/event/2016/08/20160811dkfe/event.aspx',
@@ -98,7 +98,7 @@ export const worksData =[{
     finishDate: 201608,
     type:"P",
     role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:'회사내에서 처음으로 canvas에 순수 자바스크립트를 사용하여 인트로 페이지의 애니메이션 제작 기존에 모듈화 하였던 티징 페이지 코드를 좀더 수정하여 기능과 선언의 분리를 좀더 명확하게 함 코드의 관리가 좀더 쉬워짐'
+    desc:'회사내에서 처음으로 canvas에 순수 자바스크립트를 사용하여 인트로 페이지의 애니메이션 제작 기존에 모듈화 하였던 티징 페이지 코드를 좀더 수정하여 기능과 선언의 분리를 좀더 명확하게 함 코드의 관리가 좀더 쉬워졌다.'
 }, {
     name: '자동차 구현(html,css3,javascript)',
     url: 'http://richg0ld.github.io/study/car/car.html',
@@ -107,28 +107,18 @@ export const worksData =[{
     startDate: 201607,
     finishDate: 201608,
     type:"E",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:'회사내에서 처음으로 canvas에 순수 자바스크립트를 사용하여 인트로 페이지의 애니메이션 제작 기존에 모듈화 하였던 티징 페이지 코드를 좀더 수정하여 기능과 선언의 분리를 좀더 명확하게 함 코드의 관리가 좀더 쉬워짐'
+    role:'html, javascript를 이용한 프론트 인터렉션 개발',
+    desc:'일반 html의 돔조작을 이용한 자동차 구현체이며, 프로토타입과 클래스패턴을 이용해, 자동차의 각각의 부품들의 기능과 구현체를 분리 하였으며, DI를 이용해 다량의 차량을 찍어 낼 수 있는 코드를 만들었습니다.'
 }, {
-    name: '게임 캐릭터 움직임 구현자동차 구현(canvas,javascript)',
+    name: '게임 캐릭터 움직임 구현(canvas,javascript)',
     url: 'http://richg0ld.github.io/study/character2/char2.html',
     img: require('./../images/nara.gif'),
     thumbImg: '',
     startDate: 201505,
     finishDate: 201506,
     type:"E",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:'회사내에서 처음으로 canvas에 순수 자바스크립트를 사용하여 인트로 페이지의 애니메이션 제작 기존에 모듈화 하였던 티징 페이지 코드를 좀더 수정하여 기능과 선언의 분리를 좀더 명확하게 함 코드의 관리가 좀더 쉬워짐'
-}, {
-    name: '게임 캐릭터 움직임 구현자동차 구현(canvas,javascript)',
-    url: 'http://richg0ld.github.io/study/character2/char2.html',
-    img: require('./../images/nara.gif'),
-    thumbImg: '',
-    startDate: 201607,
-    finishDate: 201608,
-    type:"E",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:'회사내에서 처음으로 canvas에 순수 자바스크립트를 사용하여 인트로 페이지의 애니메이션 제작 기존에 모듈화 하였던 티징 페이지 코드를 좀더 수정하여 기능과 선언의 분리를 좀더 명확하게 함 코드의 관리가 좀더 쉬워짐'
+    role:'canvas, Typescript, Javascript를 이용하여 작업',
+    desc:'캔버스에 이미지 스프라이트 기법을 이용한 게임 캐릭터 움직임을 구현하였다. Typescript를 처음 사용하였으며, 타입 선언이 얼마나 많은 예외상황을 막을 수 있는지 알 수 있는 작업물 이었습니다.'
 }, {
     name: 'slider 라이브러리 제작',
     url: 'http://richg0ld.github.io/study/slider/slider.html',
@@ -137,8 +127,8 @@ export const worksData =[{
     startDate: 201605,
     finishDate: 201606,
     type:"E",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:'회사내에서 처음으로 canvas에 순수 자바스크립트를 사용하여 인트로 페이지의 애니메이션 제작 기존에 모듈화 하였던 티징 페이지 코드를 좀더 수정하여 기능과 선언의 분리를 좀더 명확하게 함 코드의 관리가 좀더 쉬워짐'
+    role:'Jquery, Javascript를 이용한 프론트 인터렉션 개발',
+    desc:'회사에서 사용할 슬라이드 라이브러리를 제작하였으며, 제이쿼리를 이용한 슬라이드 라이브러리 입니다. 제이쿼리를 이용하여 신입이나 초보들이 좀 더 쉽게 사용 가능하며 자연스럽게 하위 브라우저에서도 돌아갈 수 있게 되었습니다.'
 }, {
     name: '세번째 중력 및 충돌 효과 작업',
     url: 'http://richg0ld.github.io/study/gravity/index.html',
@@ -147,8 +137,8 @@ export const worksData =[{
     startDate: 201605,
     finishDate: 201606,
     type:"E",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:'회사내에서 처음으로 canvas에 순수 자바스크립트를 사용하여 인트로 페이지의 애니메이션 제작 기존에 모듈화 하였던 티징 페이지 코드를 좀더 수정하여 기능과 선언의 분리를 좀더 명확하게 함 코드의 관리가 좀더 쉬워짐'
+    role:'canvas, javascript 이용한 작업',
+    desc:'첫번째와 두번쨰의 시항착오 후 다양한 자료들을 참고하여 보다 자연스러운 효과를 구현해 내었다.'
 }, {
     name: '두번째 중력 및 충돌 효과 작업',
     url: 'http://richg0ld.github.io/study/collide/collide2.html',
@@ -167,8 +157,8 @@ export const worksData =[{
     startDate: 201511,
     finishDate: 201512,
     type:"E",
-    role:'canvas, javascript를 이용한 프론트 인터렉션 개발',
-    desc:'회사내에서 처음으로 canvas에 순수 자바스크립트를 사용하여 인트로 페이지의 애니메이션 제작 기존에 모듈화 하였던 티징 페이지 코드를 좀더 수정하여 기능과 선언의 분리를 좀더 명확하게 함 코드의 관리가 좀더 쉬워짐'
+    role:'canvas, javascript를 이용한 작업',
+    desc:'중력에 대한 내용보다는 물체와 물체들의 충돌에 대한 작업을 '
 }, {
     name: '홈페이지 리엑트 작업',
     url: 'https://github.com/richg0ld/portfolio',
